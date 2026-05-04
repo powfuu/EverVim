34873f8be6b29f3a7a7594f6a5bc29c059d07ec1
Everit <emolero@profile.es>
2026-05-04 09:32:16 +0200

    feat: increase float terminal size to 90% width and 80% height

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -68,4 +68,15 @@ M.ui = {
   },
 }
 
+M.term = {
+  float = {
+    relative = "editor",
+    row = 0.1,
+    col = 0.05,
+    width = 0.9,
+    height = 0.8,
+    border = "single",
+  },
+}
+
 return M
